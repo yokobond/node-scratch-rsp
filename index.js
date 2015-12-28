ef463092986170d629ef349a47bbd911d283a908
yokobond <koji.yokokawa@gmail.com>
2015-12-28 16:44:10 +0900

    Add sensor-update, broadcast send/receive

---
+++ index.js
@@ -0,0 +1,97 @@
+/**
+ * Connector to Scratch via Remote-Sensor-Protocol.
+ * 
+ * @module node-scratch-rsp
+ * @author Koji Yokokawa
+ * @version 0.0.1 
+ */
+var net = require('net');
+
+var scratchpPort = 42001;
+
+/**
+ * Connect host on scratch port and return a Socket instance. 
+ * 
+ * @param {String} host Host address of Scratch to connect
+ * @param {function} connectListener listener of connected event
+ * @returns {net.Socket} socket for Scratch
+ */
+exports.createConnection = function (host, connectListener) {
+	var scratchSocket;
+	scratchSocket = net.createConnection(scratchpPort, host, connectListener);
+	asScratchSocket(scratchSocket);
+	return scratchSocket;
+};
+
+var asScratchSocket = function (socket) {
+
+	/**
+	 * Send sensor-update message to the Scratch. 
+	 * Returns true if the entire data was flushed successfully to the kernel buffer. 
+	 * Returns false if all or part of the data was queued in user memory. 
+	 * 'drain' will be emitted when the buffer is again free.
+	 * 
+	 * @param {Map} sensorsMap Map of "sensor-name -> value" to be sent.
+	 * @param {function} callback It will be executed when the data is finally written out. 
+	 * @returns {Boolean} Returns true when the data was flushed successfully, or false. 
+	 */
+	socket.sensorUpdate = function (sensorsMap, callback) {
+		var message = 'sensor-update';
+		for (var key of sensorsMap.keys()) {
+			message += ' ';
+			message += '\"';
+			message += key.replace(/"/g, '""');
+			message += '\" ';
+			message += sensorsMap.get(key).toString();
+		}
+		return this.writeScratchMessage(message, callback);
+	};
+
+	/**
+	 * Send broadcast message to the Scratch. 
+	 * Returns true if the entire data was flushed successfully to the kernel buffer. 
+	 * Returns false if all or part of the data was queued in user memory. 
+	 * 'drain' will be emitted when the buffer is again free.
+	 * 
+	 * @param {String} subject Subject to be broadcasted.
+	 * @param {function} callback It will be executed when the data is finally written out. 
+	 * @returns {Boolean} Returns true when the data was flushed successfully, or false. 
+	 */
+	socket.broadcast = function (subject, callback) {
+		var message = 'broadcast "';
+		message += subject.replace(/"/g, '""');
+		message += '"';
+		return this.writeScratchMessage(message, callback);
+	};
+
+	/**
+	 * Write Scratch-Remote-Sensor message to the socket. 
+	 * Returns true if the entire data was flushed successfully to the kernel buffer. 
+	 * Returns false if all or part of the data was queued in user memory. 
+	 * 'drain' will be emitted when the buffer is again free.
+	 * 
+	 * @param {String} message Message to be sent.
+	 * @param {function} callback It will be executed when the data is finally written out.
+	 * @returns {Boolean} Returns true when the data was flushed successfully, or false. 
+	 */
+	socket.writeScratchMessage = function (message, callback) {
+		var buff = new Buffer(message.length + 4);
+		buff.writeUInt32BE(message.length);
+		buff.write(message, 4, 'utf8');
+		return this.write(buff, 'utf8', callback);
+	};
+
+	socket.on('data', function (dataBuffer) {
+		var messageSize = dataBuffer.readUInt32BE(0);
+		var message = dataBuffer.toString('utf8', 4, 4 + messageSize);
+		if (message.lastIndexOf('broadcast', 0) === 0) {
+			var subject = message.substring(9, message.length);
+			subject = subject.trim();
+			subject = subject.replace(/"(?!")/g, '');
+			subject = subject.replace(/""/g, '"');
+			socket.emit('broadcast', subject);
+		}
+	});
+	return socket;
+}
+
---
+++ specs/node-scratch.js
@@ -0,0 +1,128 @@
+var chai = require('chai');
+var sinon = require('sinon');
+var NodeScratch = require('../index');
+var net = require('net');
+
+describe('Send Message', function () {
+
+	before(function (done) {
+		done();
+	});
+
+	it('createConnection', function (done) {
+		var scratchSocket;
+		var host = 'localhost';
+		var scratchPort = 42001;
+		var scratchMock;
+		scratchMock = net.createServer(function (socket) { });
+		scratchMock.listen(scratchPort, host);
+		var connectListner = function () {
+			scratchSocket.destroy();
+			scratchMock.close(function (err) {
+				done(err);
+			});
+		}
+		// give a time to avoid 'error connect ECONNREFUSED'
+		setTimeout(function () {
+			scratchSocket = NodeScratch.createConnection(host, connectListner);
+		}, 1);
+	});
+
+	it('sensor-update', function (done) {
+		var host = 'localhost';
+		var scratchPort = 42001;
+		var scratchMock;
+		scratchMock = net.createServer(function (socket) {
+			expect(socket).instanceof(net.Socket);
+			socket.on('data', function (buffer) {
+				assert.equal(buffer.readUInt32BE(0), messageSize);
+				assert.equal(buffer.toString('utf8', 4, messageSize), scratchMessage);
+			})
+		});
+		scratchMock.listen(scratchPort, host);
+		var sensorMap = new Map([['note', 60], ['seconds', 0.1], ['shift "tone"', -1]]);
+		var scratchMessage = 'sensor-update "note" 60 "seconds" 0.1 "shift ""tone""" -1';
+		var messageSize = scratchMessage.length;
+		var connectedSpy = sinon.spy();
+		var connectListner = function () {
+			connectedSpy();
+		};
+		// give a time to avoid 'error connect ECONNREFUSED'
+		setTimeout(function () {
+			var scratchSocket = NodeScratch.createConnection(host, connectListner);
+			var sentCallback = function () {
+				assert.equal(connectedSpy.called, true);
+				scratchSocket.destroy();
+				scratchMock.close(function (err) {
+					done(err);
+				});
+			};
+			var flushed = scratchSocket.sensorUpdate(sensorMap, sentCallback);
+			assert.equal(flushed, true);
+		}, 1);
+	});
+
+	it('broadcast', function (done) {
+		var host = 'localhost';
+		var scratchPort = 42001;
+		var scratchMock;
+		scratchMock = net.createServer(function (socket) {
+			expect(socket).instanceof(net.Socket);
+			socket.on('data', function (buffer) {
+				assert.equal(buffer.readUInt32BE(0), messageSize);
+				assert.equal(buffer.toString('utf8', 4, messageSize), scratchMessage);
+			})
+		});
+		scratchMock.listen(scratchPort, host);
+		var broadcastSubject = 'play "note"';
+		var scratchMessage = 'broadcast "play ""note"""';
+		var messageSize = scratchMessage.length;
+		var connectedSpy = sinon.spy();
+		var connectListner = function () {
+			connectedSpy();
+		};
+		// give a time to avoid 'error connect ECONNREFUSED'
+		setTimeout(function () {
+			var scratchSocket = NodeScratch.createConnection(host, connectListner);
+			var sentCallback = function () {
+				assert.equal(connectedSpy.called, true);
+				scratchSocket.destroy();
+				scratchMock.close(function (err) {
+					done(err);
+				});
+			};
+			assert.equal(scratchSocket.broadcast(broadcastSubject, sentCallback), true);
+		}, 1);
+	});
+
+	it('broadcast receive', function (done) {
+		var host = 'localhost';
+		var scratchPort = 42001;
+		var scratchMock;
+		var broadcastSubject = 'play "note"';
+		var scratchMessage = 'broadcast "play ""note"""';
+		scratchMock = net.createServer(function (socket) {
+			expect(socket).instanceof(net.Socket);
+			var sentCallback = function () { };
+			var buff = new Buffer(scratchMessage.length + 4);
+			buff.writeUInt32BE(scratchMessage.length);
+			buff.write(scratchMessage, 4, 'utf8');
+			assert.equal(socket.write(buff, 'utf8', sentCallback), true);
+		});
+		scratchMock.listen(scratchPort, host);
+		// give a time to avoid 'error connect ECONNREFUSED'
+		setTimeout(function () {
+			var connectListner = function () { };
+			var scratchSocket = NodeScratch.createConnection(host, connectListner);
+			var broadcastListener = function (subject) {
+				assert.equal(subject, broadcastSubject);
+				scratchSocket.destroy();
+				scratchMock.close(function (err) {
+					done(err);
+				});
+			};
+			scratchSocket.on('broadcast', broadcastListener);
+		}, 1);
+	});
+
+})
